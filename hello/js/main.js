9b206ea16cde63a7ff40f1b32e53a21f487c0829
💖 <moengchoengbo@0-2.local>
2022-06-10 16:44:52 +0900

    메지

---
+++ hello/js/main.js
@@ -0,0 +1,26 @@
+$(function(){
+
+$('.sliders').slick({
+    autoplay: true,               //자동슬라이드
+    autoplaySpeed: 2000,          // 슬라이드 전환 속도 
+    //fade: true,                   // 튜명도 전환
+    speed: 2000,                  // 이미지 전환시 속도
+    dots:true,
+    arrows: true,
+    //infinite:true,
+    easing:'swing',
+    initialSlide:4,
+    pauseOnFosuc:false,
+    pauseOnHover:false,
+    swipe:false,
+    //vertical:true,
+
+
+
+
+});
+
+
+
+
+})
